4757ac4996a041e4bb9002cd8f8328a0505c2656
少宝 黄 <3370539497@qq.com>
2025-10-07 07:42:41 +0800

    最终版

--- realize.c
+++ realize.c
@@ -230,11 +230,58 @@ void Move_Snake_Body(Maintain_Game_Information* game_info)
 	//当ate_food为true时，表示吃到食物
 	if (ate_food)
 	{
+		//更新分数
+		game_info->score += 1; //每吃到一个食物，分数增加1
+
 		if (!Insert_Single_Bnake_Body_Node_At_Head(game_info, new_head_x, new_head_y, 3)) //在蛇身链表头尾插新蛇头
 		{
 			Free_All_Dynamic_Memory(game_info); //释放所有动态分配的内存
 			exit(-1); //插入失败，退出程序
 		}
+
+		//重新生成食物位置
+		int food_x = rand() % 26 + 1; //生成1到27之间的随机X坐标
+		int food_y = rand() % 26 + 1; //生成1到27之间的随机Y坐标
+		//判断食物位置是否与蛇身重合，若重合则重新生成位置
+		//判断食物位置是否与食物重合，若重合则重新生成位置
+		while (true)
+		{
+			bool is_overlap = false; 
+
+			//判断是否与蛇身重合
+			Single_Bnake_Body_Node* current = game_info->head;
+			while (current)
+			{
+				if (current->x == food_x && current->y == food_y)
+				{
+					is_overlap = true; //位置重合
+					break;
+				}
+				current = current->next; //移动到下一个节点
+			}
+
+			//判断是否与食物重合
+			Food_Node* food_current = game_info->food_tail;
+			while (food_current)
+			{
+				if (food_current->x == food_x && food_current->y == food_y)
+				{
+					is_overlap = true; //位置重合
+					break;
+				}
+				food_current = food_current->next; //移动到下一个节点
+			}
+			if (!is_overlap)
+			{
+				break; //位置不重合，跳出循环
+			}
+			//重新生成食物位置
+			food_x = rand() % 26 + 1;
+			food_y = rand() % 26 + 1;
+		}
+		//更新新食物位置
+		food_current->x = food_x;
+		food_current->y = food_y;
 	}
 	else//当ate_food为false时，表示未吃到食物
 	{
@@ -306,6 +353,8 @@ void Game_Init(Maintain_Game_Information* game_info)
 
 	Free_All_Dynamic_Memory(game_info);//释放之前的动态内存，防止内存泄漏
 
+	fflush(stdin);//清理输入缓冲区
+
 	//创建蛇身
 	for (int i = 0; i < game_info->length; i++)
 	{
@@ -365,6 +414,8 @@ void Game_Init(Maintain_Game_Information* game_info)
 	game_info->is_game_over = false;	//初始游戏未结束
 	game_info->state = NORMAL;			//将游戏状态设置为正常
 
+	GetAsyncKeyState(VK_ESCAPE);		//清理ESC键输入缓冲区
+
 	//刷新控制台显示
 	Console_Refresh(game_info);
 }
@@ -393,14 +444,14 @@ void Output_Information(Maintain_Game_Information* game_info)
 		while (choice != 1 && choice != 2)
 		{
 
-			Locate_Cursor_Position((100 - 36) / 2, 10, game_info->houtput);
+			Locate_Cursor_Position((100 - 32) / 2, 10, game_info->houtput);
 			printf("是否自定义初始蛇长度与食物数量？");
-			Locate_Cursor_Position((100 - 20) / 2, 12, game_info->houtput);
-			printf("1. 是 (Y / N)");
-			Locate_Cursor_Position((100 - 20) / 2, 14, game_info->houtput);
-			printf("2. 否 (N / N)");
-			Locate_Cursor_Position((100 - 21) / 2, 16, game_info->houtput);
-			printf("请选择 (1 / 2): ");
+			Locate_Cursor_Position((100 - 5) / 2, 12, game_info->houtput);
+			printf("1. 是");
+			Locate_Cursor_Position((100 - 5) / 2, 14, game_info->houtput);
+			printf("2. 否");
+			Locate_Cursor_Position((100 - 15) / 2, 16, game_info->houtput);
+			printf("请输入 (1 / 2): ");
 			scanf("%d", &choice);
 
 			if (choice < 1 || choice>2)
@@ -465,17 +516,24 @@ void Output_Information(Maintain_Game_Information* game_info)
 			Game_Init(game_info); //游戏初始化
 		}
 	}
-	else if (game_info->state == NORMAL)//正常状态
+	//正常状态
+	else if (game_info->state == NORMAL)
 	{
 
 		if (GetAsyncKeyState(0x50)) //按P键暂停
 		{
 			game_info->state = PAUSE;
+
+			//刷新控制台显示	
+			Console_Refresh(game_info);
+
 			Locate_Cursor_Position(60, 27, game_info->houtput);
 			printf("游戏已暂停");
 			Locate_Cursor_Position(60, 28, game_info->houtput);
 			printf("按 P 键继续游戏，按 Esc 键退出游戏");
 
+			//延迟，防止按键过快导致状态切换错误
+			Sleep(300);
 			return;
 		}
 		
@@ -499,10 +557,18 @@ void Output_Information(Maintain_Game_Information* game_info)
 				if (GetAsyncKeyState(VK_LEFT))
 				{
 					game_info->next_dir = LEFT;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_UP);
+					GetAsyncKeyState(VK_DOWN);
+					GetAsyncKeyState(VK_RIGHT);
 				}
 				if (GetAsyncKeyState(VK_RIGHT))
 				{
 					game_info->next_dir = RIGHT;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_UP);
+					GetAsyncKeyState(VK_DOWN);
+					GetAsyncKeyState(VK_LEFT);
 				}
 				break;
 
@@ -510,10 +576,18 @@ void Output_Information(Maintain_Game_Information* game_info)
 				if (GetAsyncKeyState(VK_LEFT))
 				{
 					game_info->next_dir = LEFT;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_UP);
+					GetAsyncKeyState(VK_DOWN);
+					GetAsyncKeyState(VK_RIGHT);
 				}
 				if (GetAsyncKeyState(VK_RIGHT))
 				{
 					game_info->next_dir = RIGHT;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_UP);
+					GetAsyncKeyState(VK_DOWN);
+					GetAsyncKeyState(VK_LEFT);
 				}
 				break;
 
@@ -521,10 +595,18 @@ void Output_Information(Maintain_Game_Information* game_info)
 				if (GetAsyncKeyState(VK_UP))
 				{
 					game_info->next_dir = UP;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_LEFT);
+					GetAsyncKeyState(VK_DOWN);
+					GetAsyncKeyState(VK_RIGHT);
 				}
 				else if (GetAsyncKeyState(VK_DOWN))
 				{
 					game_info->next_dir = DOWN;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_LEFT);
+					GetAsyncKeyState(VK_UP);
+					GetAsyncKeyState(VK_RIGHT);
 				}
 				break;
 
@@ -532,10 +614,18 @@ void Output_Information(Maintain_Game_Information* game_info)
 				if (GetAsyncKeyState(VK_UP))
 				{
 					game_info->next_dir = UP;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_LEFT);
+					GetAsyncKeyState(VK_DOWN);
+					GetAsyncKeyState(VK_RIGHT);
 				}
 				else if (GetAsyncKeyState(VK_DOWN))
 				{
 					game_info->next_dir = DOWN;
+					//清理其他三个方向的按键缓存
+					GetAsyncKeyState(VK_LEFT);
+					GetAsyncKeyState(VK_UP);
+					GetAsyncKeyState(VK_RIGHT);
 				}
 				break;
 
@@ -548,6 +638,8 @@ void Output_Information(Maintain_Game_Information* game_info)
 		//检查是否达到移动时间间隔
 		if (game_info->current_time - game_info->last_update_time >= (ULONGLONG)game_info->sleep_time)
 		{
+
+
 			//速度控制
 			if (GetAsyncKeyState(VK_F3)) //按F3加速
 			{
@@ -573,10 +665,51 @@ void Output_Information(Maintain_Game_Information* game_info)
 			//刷新控制台显示	
 			Console_Refresh(game_info);
 		}
-
 	}
+	//蛇撞墙或撞自己状态
+	else if (game_info->state == KILL_BOUNDARY || game_info->state == KILL_SELF)
+	{
+		//提示游戏结束，并询问用户是否重新开始游戏
+		Locate_Cursor_Position(26, 12, game_info->houtput);
+		printf("游戏结束!");
+		Locate_Cursor_Position(11, 15, game_info->houtput);
+		printf("按 R 键重新开始游戏，按 Esc 键退出游戏");
 
-	//后续输出信息和提示代码待补充
+		//清除按键缓存
+		GetAsyncKeyState(0x52); 
+		GetAsyncKeyState(VK_ESCAPE);
+
+		while (true)
+		{
+			if (GetAsyncKeyState(0x52)) //按R键重新开始
+			{
+				game_info->state = NOT_START; //将游戏状态设置为未开始
+
+				system("cls"); //清屏
+
+				//清理输入缓冲区
+				fflush(stdin);
+
+				//清理所有输入缓存
+				while (kbhit())
+				{
+					getch();
+				}
+				return;
+			}
+			if (GetAsyncKeyState(VK_ESCAPE)) //按Esc键退出
+			{
+				game_info->is_game_over = true;
+				Free_All_Dynamic_Memory(game_info);//释放所有动态分配的内存
+				Locate_Cursor_Position((100 - 10) / 2, 15, game_info->houtput);
+				printf("游戏已退出");
+				Locate_Cursor_Position((100 - 28) / 2, 17, game_info->houtput);
+				printf("感谢您的游玩，欢迎下次再来!");
+				Locate_Cursor_Position(0, 28, game_info->houtput);
+				exit(0); //退出程序
+			}
+		}
+	}
 }
 
 //释放所有动态分配的内存
--- test.c
+++ test.c
@@ -16,20 +16,35 @@ int main()
 	{
 		switch (game_info.state)
 		{
-
+			case NOT_START://未开始状态
 			case NORMAL://正常状态
+			case KILL_BOUNDARY://撞墙状态
+			case KILL_SELF://撞自己状态
 				Output_Information(&game_info);
 				break;
 
 			case PAUSE://暂停状态
 
-				break;
+				if (GetAsyncKeyState(0x50)) //按P键暂停
+				{
+					game_info.state = NORMAL;
+					Sleep(300); //防止按键过快导致状态切换异常
+				
+					Console_Refresh(&game_info); //刷新控制台显示
+				}
 
-			case KILL_BOUNDARY://撞墙状态
+				if (GetAsyncKeyState(VK_ESCAPE)) //按Esc键退出
+				{
+					game_info.is_game_over = true;
 
-				break;
+					Free_All_Dynamic_Memory(&game_info);//释放所有动态分配的内存
+					Locate_Cursor_Position((100 - 12) / 2, 15, game_info.houtput);
+					printf("游戏已退出");
+					Locate_Cursor_Position((100 - 21) / 2, 17, game_info.houtput);
+					printf("感谢您的游玩，欢迎下次再来!");
 
-			case KILL_SELF://撞自己状态
+					exit(0); //退出程序
+				}
 
 				break;
 
